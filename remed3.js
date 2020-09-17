e578217c49558fd84771c8e34271b501e8904fbc
ninjaa <alkhudrisaid83@gmail.com>
2020-09-17 09:42:58 +0700

    first commit

---
+++ remed3.js
@@ -0,0 +1,7 @@
+function luas(panjang, lebar) {
+  var luas = panjang * lebar;
+  return luas;
+}
+document.write("panjang : 10 <br>");
+document.write("lebar : 5 <br>");
+document.write("luas persegi panjang :" + luas(10, 5));
